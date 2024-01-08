b2e41ecbf057a14a38406b204ba456c3f9f37a24
tcorrea <thiagocorreacontato@gmail.com>
2024-01-08 11:27:09 -0300

    test: hello world

---
+++ helloworld/hello_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// testing.TB which is an interface that *testing.T and *testing.B both satisfy
+	// t.Helper() is needed to tell the test suite that this method is a helper
+	t.Helper()
+	if got != want {
+		// O que é %q?
+		// https://pkg.go.dev/fmt#hdr-Printing
+		t.Errorf("got %q want %q", got, want)
+	}
+}
