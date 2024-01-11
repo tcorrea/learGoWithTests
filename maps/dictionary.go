7f6d0472b17ed867a1cf784d55c4ad2eb2d900d8
tcorrea <thiagocorreacontato@gmail.com>
2024-01-11 08:02:50 -0300

    feat: maps

---
+++ maps/dictionary.go
@@ -0,0 +1,60 @@
+package maps
+
+type (
+	Dictionary    map[string]string
+	DictionaryErr string
+)
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+// Aqui, um método chamado Error é definido para o tipo DictionaryErr.
+// Esse método permite que DictionaryErr satisfaça a interface error em Go,
+// pois a interface error exige apenas um método Error() string.
+// Este método retorna a representação da string do erro.
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(key, value string) error {
+	_, err := d.Search(key)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[key] = value
+	default:
+		return err
+	}
+	return nil
+}
+
+// TODO
+func (d Dictionary) Delete(key string) {
+	delete(d, key)
+}
